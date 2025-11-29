7b70b684abc55070b642ff5553e55c8e7f82493e
Michael Janeway <michael.janeway03@gmail.com>
2025-11-29 11:13:47 -0500

    Override ALL unbind functions

--- src/gui/ParaduxWindow.py
+++ src/gui/ParaduxWindow.py
@@ -34,7 +34,15 @@ class ParaduxGui(tk.Tk):
 
     def on_err(self, message: str):
         print(message)
-        
+
     def unbind(self, sequence: str, funcid: str | None = None) -> None:
         print("CRAZY DEBUG UNBIND: ", sequence, funcid)
         return super().unbind(sequence, funcid)
+
+    def unbind_all(self, sequence: str) -> None:
+        print("CRAZY UNBIND ALL", sequence)
+        return super().unbind_all(sequence)
+
+    def unbind_class(self, className: str, sequence: str) -> None:
+        print("CRAZY UNBIND CLASS", className, sequence)
+        return super().unbind_class(className, sequence)
